c81e4d8397323232c6afcdd8f0f4f1593a6e30df
wenhuixu <wenhuixu2017@outlook.com>
2023-05-12 14:24:08 +0930

    updating app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from routes.memes_routes import memes_routes
+from routes.users_routes import users_routes
+from routes.sessions_routes import sessions_routes
+
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "default pretend key")
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+app.register_blueprint(memes_routes)
+app.register_blueprint(users_routes)
+app.register_blueprint(sessions_routes)
